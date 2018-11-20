4f1c89a70b86d99f1251b2b6b7e2cced8119e07a
wulening <wulening@foxmail.com>
2018-11-20 21:13:10 +0800

    综合练习

---
+++ src/practice/Animal.java
@@ -0,0 +1,16 @@
+package practice;
+
+public abstract class Animal {
+    protected int legs;
+
+    protected Animal(int legs) {
+        this.legs = legs;
+    }
+
+
+    public abstract void eat();
+
+    public void walk(){
+        System.out.println("用"+legs+"走路");
+    }
+}
