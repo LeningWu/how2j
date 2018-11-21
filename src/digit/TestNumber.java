7f127319b4571f7b0bcf5da16cff0961232a659e
wulening <wulening@foxmail.com>
2018-11-21 16:14:02 +0800

    test

---
+++ src/digit/TestNumber.java
@@ -0,0 +1,18 @@
+package digit;
+
+public class TestNumber {
+    public static void main(String[] args) {
+        /*
+        int i=5;
+
+        Integer it = new Integer(i);
+      //  int i2 = it.intValue();
+        System.out.println(it instanceof Number);
+    }
+    */
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println((Integer.MIN_VALUE));
+
+    }
+}
