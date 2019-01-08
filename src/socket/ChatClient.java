0f8095c10752eaa9da78f6dd4a56c49ee67c54f1
wulening <wulening@foxmail.com>
2019-01-08 11:20:21 +0800

    聊天客户端

---
+++ src/socket/ChatClient.java
@@ -0,0 +1,32 @@
+package socket;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ChatClient {
+    public static void main(String[] args){
+        try {
+            Socket s=new Socket("127.0.0.1",8888);
+
+            OutputStream os=s.getOutputStream();
+            DataOutputStream dos=new DataOutputStream(os);
+
+            InputStream is=s.getInputStream();
+            DataInputStream dis=new DataInputStream(is);
+
+            while (true){
+                Scanner sc=new Scanner(System.in);
+                String str=sc.next();
+                dos.writeUTF(str);
+                String msg=dis.readUTF();
+                System.out.println("收到服务端信息："+msg);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
