3234ace6d9c364b98753e75abf49c7242747fd60
wulening <wulening@foxmail.com>
2019-01-08 11:20:56 +0800

    聊天服务

---
+++ src/socket/ChatServer.java
@@ -0,0 +1,32 @@
+package socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatServer {
+    public static void main(String[] args){
+        try {
+            ServerSocket ss=new ServerSocket(8888);
+
+            System.out.println("监听在端口号8888");
+            Socket s=ss.accept();
+            InputStream is=s.getInputStream();
+            DataInputStream dis=new DataInputStream(is);
+
+            OutputStream os=s.getOutputStream();
+            DataOutputStream dos=new DataOutputStream(os);
+
+            while (true){
+                String msg=dis.readUTF();
+                System.out.println("收到客户端信息"+msg);
+                Scanner sc=new Scanner(System.in);
+                String str=sc.next();
+                dos.writeUTF(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
